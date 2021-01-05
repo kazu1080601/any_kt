ae2910df080c1160f68bf1e5c6e89ca624fbe960
kazu1080601 <kazu1080601@gmail.com>
2021-01-05 16:00:37 +0900

    新規登録バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: { with: /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}\z/, message: 'complexity requirement not met. Length should be 6 characters minimum and include at least one letter and one number.(Excluding full-width)'}
+    validates :prefecture_id, numericality: { other_than: 1, message: 'select' }
+    validates :city
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_many :valuations
+  belongs_to :prefecture
+
 end
