9109beac58ff9efa9e1fd616c0315e861dfb23b8
kazu1080601 <kazu1080601@gmail.com>
2021-01-21 12:01:16 +0900

    ログイン、ログアウト後の画面遷移先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :prefecture, :prefecture_id, :city])
   end
 
+  # ログイン後の画面遷移先をrootからrecommendations_pathに変更
+  def after_sign_in_path_for(resource)
+    # flash[:notice] = "ログインに成功しました"
+    recommendations_path
+  end
+
+  # ログアウト後の画面遷移先をrootからrecommendations_pathに変更
+  def after_sign_out_path_for(resource)
+      # flash[:alert] = "ログアウトしました"
+      recommendations_path
+  end
+
 end
